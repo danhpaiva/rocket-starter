321f5c4e8dc0725bc6ceea6ada7a2bde2da69936
Daniel Paiva <danpaiva@live.com>
2020-07-22 23:34:55 -0300

    Exercício 01

---
+++ curso_JavaScript/1_introducao/desafio/exercicio01.js
@@ -0,0 +1,19 @@
+var endereco = {
+    rua: "Rua dos Pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+};
+
+function exibirMensagem(endereco) {
+    return (
+        "O usuário mora em " + endereco.cidade + " / " +
+        endereco.uf + ", no bairro " + endereco.bairro + ', na rua "' + endereco.rua +
+        '" com nº ' + endereco.numero + "."
+    );
+}
+
+document.write("<br>");
+document.write(exibirMensagem(endereco));
+console.log(exibirMensagem(endereco));
