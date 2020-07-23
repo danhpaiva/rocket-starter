b1e1196fd75d8b89d6d24f1711b0f1ff6d5a1a60
Daniel Paiva <danpaiva@live.com>
2020-07-23 00:10:57 -0300

    Exercício 04

---
+++ curso_JavaScript/1_introducao/desafio/exercicio04.js
@@ -0,0 +1,19 @@
+function experiencia(anos) {
+    if (anos <= 1) {
+        document.write("Iniciante");
+    } else if (anos <= 3) {
+        document.write("Intermediário");
+    } else if (anos <= 6) {
+        document.write("Avançado");
+    } else {
+        document.write("Jedi Master");
+    }
+}
+
+var anosEstudo = 7;
+
+experiencia(anosEstudo);
+    // De 0-1 ano: Iniciante
+    // De 1-3 anos: Intermediário
+    // De 3-6 anos: Avançado
+    // De 7 acima: Jedi Master
